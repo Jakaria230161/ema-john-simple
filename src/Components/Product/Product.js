4f3de645244fd3bb04e895a23fd542bf29677b61
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-08 21:06:49 +0600

    product button section added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {category,name, img, seller, price, ratings } = props.product;
+    const {category,name, img, seller, price, ratings,quantity, shipping } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
             <p className='product-name'>{name}</p>
+            <p>Category: {category}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p>Ratings: {ratings}</p>
+            <p>Quantity: {quantity}</p>
+            <p>Shipping: {shipping}</p>
+            </div>
+
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
