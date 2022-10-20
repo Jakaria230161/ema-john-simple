a7b6a86983d1fbb2ecbaad5a9ac71878cd55909a
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-20 23:07:59 +0600

    login form style added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login form.</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Login</h1>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="name" id="" placeholder='Enter your email' required></input>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='email'>Password</label>
+                    <input type="password" name="password" id="" placeholder='Enter your Password' required></input>
+                </div>
+                <input className='btn-submit' type="submit" value="Login"></input>
+            </form>
+            <p>New to Ema john <Link to="/signup">Create a New Account</Link></p>
         </div>
     );
 };
